a9c816c4db73441fda8369e421b59fc67d5e32b0
Qohat Pretel <qpretel@starbucks.com>
2021-04-25 19:24:47 -0500

    Fsharp FileRW

--- src/Infra/Files.fs
+++ src/Infra/Files.fs
@@ -5,22 +5,33 @@ open System.IO
 module Files =
 
     type Folder = { path : string } 
-        //let filenameArray : string array = Directory.GetFiles(path)
-        //member this.FileArray = Array.map (fun elem -> new File(elem, this)) filenameArray
-    and File = {filename: string; containingFolder: Folder; lines : List<string>}
-        //member this.Lines =  File.ReadAllLines(this.Name)
-    and FilePath = { path : string}    
+    type File = {filename: string; containingFolder: Folder; lines : string array}
+    type FilePath = { path : string }
+
+    let private toFile(fileName: string, folder: Folder): File =
+        {filename = fileName; containingFolder = folder; lines = File.ReadAllLines(fileName)}
 
     type FileRW =
-        abstract member Read : FilePath -> File
+        abstract member Read : FilePath -> File array
         abstract member Write : File -> unit
         abstract member Create : Folder -> FilePath
 
     type MyFileRW() =
+        member this.Read(fp) = (this :> FileRW).Read(fp)
+        member this.Write(file) = (this :> FileRW).Write(file)
+        member this.Create(folder) = (this :> FileRW).Create(folder)
         interface FileRW with
             member this.Read fp = 
-                let filenameArray = 
-                    Directory.GetFiles(path)
-                    |> Array.map (fun elem -> new File(elem, Folder path))
+                    Array.map (fun elem -> toFile(elem, {path = fp.path})) (Directory.GetFiles(fp.path))
+            member this.Create folder = 
+                if Directory.Exists(folder.path) then { path = folder.path } 
+                else
+                    Directory.CreateDirectory(folder.path)
+                    |> fun _ -> { path = folder.path }
+            member this.Write file = 
+                this.Create(file.containingFolder)
+                |> fun _ -> File.WriteAllLines(file.filename, file.lines)
+
+           
                 
 
